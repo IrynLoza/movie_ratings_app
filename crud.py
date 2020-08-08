9710351d1324a60df818b459ba8ab74fee1577ea
Iryna Loza <irynloza@gmail.com>
2020-08-07 17:01:39 -0700

    Create get_user_by_id function

--- crud.py
+++ crud.py
@@ -13,6 +13,14 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+    
+    return User.query.all() 
+
+def get_user_by_id(user_id):
+
+    return User.query.get(user_id)      
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
